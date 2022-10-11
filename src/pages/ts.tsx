431ade30152ea05ee2eb206c29ef05ba976e4f96
kyoichi <s206cg06k@yahoo.co.jp>
2022-10-11 23:54:43 +0900

    ♻️

--- src/pages/ts.tsx
+++ src/pages/ts.tsx
@@ -1,10 +1,20 @@
+import { type } from "os";
 import React from "react";
 
-export const TypeScript = () => {
+type Props = {
+  a: string;
+  b: number;
+};
+
+export const TypeScript = (Prpps: Props) => {
+  const a = "AAAA";
+  const b = 1111;
+
   return (
     <div>
       <h1>TypeScript</h1>
-      <input></input>
+      <input>{Prpps.a}</input>
+      <input>{Prpps.b}</input>
     </div>
   );
 };
